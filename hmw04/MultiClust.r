ac2be8c84893069706c8176c868adbe58586b5a8
pabloaks <juanpablorobles1@gmail.com>
2015-11-24 01:40:05 -0500

    Create MultiClust.r

---
+++ hmw04/MultiClust.r
@@ -0,0 +1,44 @@
+rm(list=ls())
+
+################# FUNCTIONS #################
+MultinomialEM <- function(H,K,tau)
+{
+	n = dim(H)[1]
+	d = dim(H)[2]
+	H = H
+	
+	t = H[sample(n,K),]
+	c = matrix(1/K,1,K)
+	c = as.vector(c)
+	a = matrix(1/K,n,K)
+
+	tolerance = 2*tau
+	while(tolerance > tau)
+	{
+		phi = exp(H%*%t(log(t)))
+		a_temp = a
+		a = c*phi/rowSums(c*phi)
+		c = colSums(a)/n
+		t = t(a)%*%H / rowSums(t(a)%*%H)
+		tolerance = norm(a - a_temp, "O")
+	}
+	m = matrix(1,n,1)
+	for(i in 1:n)
+	{
+		m[i] = which.max(a[i,])
+	}
+	return(m)
+}
+##############################################
+############### driver of algo ###############
+H<-matrix(readBin("histograms.bin", "double", 640000), 40000, 16)
+H = H + 0.01
+for(j in 3:4)
+{
+	dev.new()
+	tau = 0.01
+	K = j
+	ans = matrix(MultinomialEM(H,K,tau),200,200)
+	image(ans)
+	title(main=paste("Clusters k = ", K," -- tau = ", tau))
+}
