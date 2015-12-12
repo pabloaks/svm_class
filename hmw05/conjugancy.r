8f520d15a093163b2d6b0f6a744f57c6f4909dd4
pabloaks <juanpablorobles1@gmail.com>
2015-12-12 01:43:05 -0500

    Update conjugancy.r

--- hmw05/conjugancy.r
+++ hmw05/conjugancy.r
@@ -1,14 +1,17 @@
-rm(list=ls())
+### hw05
+
 if(! require(ggplot2))
 	{install.packages("ggplot2")}
 library("ggplot2")
-## (a) 
+
+####### (a) ####### 
+rm(list=ls())
 x = (1:800)/200
 theta = 1
 exp_fun = theta*exp(-theta*x)
-#plot(x,exp_fun, type = "l", col = "red", lwd = 3)
+plot(x,exp_fun, type = "l", col = "red", lwd = 3)
 
-##b
+####### (b) ####### 
 set_x = c(1,2,4)
 like_ind = NULL
 theta = 1
@@ -21,14 +24,8 @@ p1 <- ggplot() + geom_point(alpha = 0.4, colour = "blue", aes(x = set_x, y = as.
 p1 <- p1 + geom_point(alpha = 1.0, colour = "red", aes(x = x, y = exp_fun, size = 0.3)) + theme(legend.position="none")
 p1
 
-
-
-## g
+####### (g) ####### 
 rm(list=ls())
-if(! require(ggplot2))
-	{install.packages("ggplot2")}
-library("ggplot2")
-
 theta = (1:1600)/400
 alpha = 2.0
 beta = 0.2
